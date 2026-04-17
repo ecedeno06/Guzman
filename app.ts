ed9f49bff7891cc31c9f2653ac517940525d16c9
Edwin Cedeno <edwin.e.cedeno@gmail.com>
2026-04-17 18:12:10 -0500

    Update styles and link new CSS

--- app.ts
+++ app.ts
@@ -5,11 +5,9 @@ const emailInput = document.getElementById('email') as HTMLInputElement;
 const passwordInput = document.getElementById('password') as HTMLInputElement;
 const mensaje = document.getElementById('mensaje') as HTMLParagraphElement;
 
-
 console.log('Ya funciona');
 
 //mensaje.textContent = 'Login correcto ✅';
-
 
 form.addEventListener('submit', (event: Event) => {
     event.preventDefault();
@@ -17,12 +15,11 @@ form.addEventListener('submit', (event: Event) => {
     const email = emailInput.value;
     const password = passwordInput.value;
 
-    if (email === '' || password === ''){
+    if (email === '' || password === '') {
         mensaje.textContent = 'Todos los campos son obligatorios';
         return;
     }
 
     mensaje.textContent = 'Login correcto ✅';
-
 
 });
